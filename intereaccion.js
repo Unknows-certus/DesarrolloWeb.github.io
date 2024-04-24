ea51559fe6aaf3ff8cfc0001a0c4e9210838b902
Unknows-certus <165302079+Unknows-certus@users.noreply.github.com>
2024-04-23 19:19:53 -0500

    avance...

---
+++ intereaccion.js
@@ -0,0 +1,12 @@
+// Este es un comentario de una línea
+var x = 5; // También se puede colocar al final de una línea de código
+
+/* 
+Este es un comentario
+de múltiples líneas
+*/
+var y = 10;
+
+var suma = 5 + 3; // Esto asigna el valor 8 a la variable 'suma'
+var multiplicacion = 5 * 3; // Esto asigna el valor 15 a la variable 'multiplicacion'
+var division = 10 / 2; // Esto asigna el valor 5 a la variable 'division'
